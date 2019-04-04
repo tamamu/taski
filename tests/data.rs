ee3394a83fb824e5a65b8e223fb8d60cce386043
tamamu <tamamu.1r1s@gmail.com>
2019-04-05 00:43:20 +0900

    Add a test for parsing json

---
+++ tests/data.rs
@@ -0,0 +1,31 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_json() {
+        use std::fs::File;
+        use std::io::BufReader;
+
+        let file = File::open("taski.json").unwrap();
+        let reader = BufReader::new(file);
+
+        let u: super::Database = serde_json::from_reader(reader).unwrap();
+
+        assert!(
+            u.tasks
+                .iter()
+                .filter(|&task| task.tag == u.current_task)
+                .collect()
+                .len()
+                == 1
+        );
+
+        assert_eq!(u.tasks[0].time, 500000);
+        assert_eq!(u.tasks[0].content, "TODO1");
+        assert_eq!(u.tasks[0].done, false);
+        assert_eq!(u.tasks[0].children[0].content, "TODO2");
+        assert_eq!(u.tasks[0].children[1].content, "TODO3");
+        assert_eq!(u.tasks[1].content, "TODO4");
+    }
+}
